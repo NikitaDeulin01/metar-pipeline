e09f25d4606c99e8d3fb9c3c15d01453e1444354
Nikita <135644446+NikitaDeulin01@users.noreply.github.com>
2025-12-14 19:58:02 +0300

    Update ods_metar_latest.sql

--- dbt/models/ods/ods_metar_latest.sql
+++ dbt/models/ods/ods_metar_latest.sql
@@ -1,26 +1,19 @@
 {{ config(
     materialized='incremental',
-    unique_key='id',
-    schema='ods',
+    schema='analytics_ods',
     incremental_strategy='append',
     on_schema_change='append_new_columns'
 ) }}
 
 with source_data as (
-
     select
-        -- Берём только те id, которые полностью состоят из цифр
-        case
-            when id ~ '^[0-9]+$' then id::integer
-            else null
-        end as id,
+        id,  -- Mongo ObjectId
 
         {{ dbt_utils.star(
             from=ref('stg_metar_observations'),
             except=['id']
         ) }}
     from {{ ref('stg_metar_observations') }}
-    where id ~ '^[0-9]+$'
 )
 
 {% if not is_incremental() %}
@@ -30,10 +23,10 @@ with source_data as (
 
 {% else %}
 
-    -- Инкрементальный: только новые observed
+    -- Инкрементальный: добавляем только новые observed
     select *
     from source_data
-    where observed >= (
+    where observed > (
         select coalesce(max(observed), '1970-01-01'::timestamptz)
         from {{ this }}
     )
